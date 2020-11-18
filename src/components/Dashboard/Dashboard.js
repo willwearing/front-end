e12cbe5410842b231c02b2ae77b6efee20dae4f8
Michael Maton <mjmaton6@gmail.com>
2020-11-18 17:50:33 -0600

    dashboard css edited/media queries added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,50 +1,82 @@
 import React, { useEffect } from "react";
 import ProjectPanel from "./ProjectPanel";
-import styled from 'styled-components';
+import styled from "styled-components";
+import { v4 as uuid } from "uuid";
 
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 //import { axiosWithAuth } from './../../utils/axiosWithAuth';
 
-import { fetchProjects } from './../../actions';
+import { fetchProjects } from "./../../actions";
 
+const Dashboard = (props) => {
+  useEffect(() => {
+    props.fetchProjects();
+  }, []);
 
-const Dashboard = props => {
+  return (
+    <Header>
+      <div id="wrapper">
+        <section id="intro" className="wrapper style1 fade-up">
+          <div className="innerStuff">
+            {/*Here we will greet the user and display their details*/}
+            <p>Hello, welcome to your dashboard!</p>
+          </div>
+        </section>
+        <YourProjects>Your Projects</YourProjects>
+        {props.isLoading
+          ? "Loading Projects..."
+          : props.projects.map((proj) => {
+              return <ProjectPanel key={uuid()} project={proj} />;
+            })}
+      </div>
+    </Header>
+  );
+};
 
-    useEffect(() => {
-        props.fetchProjects();
-    }, [])
+const mapStateToProps = (state) => {
+  return {
+    projects: state.projects,
+    user: state.user,
+    isLoading: state.isLoading,
+  };
+};
 
-    return (
-        <div id="wrapper">
-            <section id="intro" className="wrapper style1 fullscreen fade-up">
-                <div className="inner">
-                    {/*Here we will greet the user and display their details*/}
-                    Hello {/*props.user.name*/}!
-                    
-                </div>
-                
-            </section>
-            <YourProjects>Your Projects</YourProjects>
-            {props.isLoading ? ('Loading Projects...') : (props.projects.map(proj => {
-                return <ProjectPanel project={proj}/>
-            }))}
-            
-        </div>
-    )
-}
-
-const mapStateToProps = state => {
-    return {
-        projects: state.projects,
-        user: state.user,
-        isLoading: state.isLoading
-    }
-}
-
-export default connect(mapStateToProps, {fetchProjects})(Dashboard)
-
+export default connect(mapStateToProps, { fetchProjects })(Dashboard);
 
 const YourProjects = styled.div`
+  font-size: 4rem;
+  background-color: #b74e91;
+  @media (max-width: 1280px) {
+    padding-left: 0;
     font-size: 3rem;
+  }
+`;
 
-`
+const Header = styled.div`
+  display: flex;
+  /* flex-direction: column; */
+  margin-left: 15rem;
+  justify-content: center;
+  align-items: center;
+  font-size: 4rem;
+  width: 100%;
+  @media (max-width: 1280px) {
+    width: 100%;
+    margin-left: 0;
+    font-size: 4rem;
+  }
+  @media (max-width: 850px) {
+    font-size: 3rem;
+  }
+  .innerStuff {
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    padding-top: 9.5rem;
+    p {
+      @media (max-width: 850px) {
+        padding: 0 10% 0 10%;
+      }
+    }
+  }
+`;
--- src/components/Dashboard/ProjectPanel.js
+++ src/components/Dashboard/ProjectPanel.js
@@ -1,38 +1,83 @@
 import React from "react";
-import { connect } from 'react-redux';
-import { deleteProject, updateProject } from './../../actions';
-import { useHistory } from 'react-router-dom';
+import { connect } from "react-redux";
+import { deleteProject, updateProject } from "./../../actions";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
 
 function ProjectPanel(props) {
-
   const history = useHistory();
 
-  const handleDelete = e => {
+  const handleDelete = (e) => {
     e.preventDefault();
     props.deleteProject(props.project.id);
-    history.push('/dashboard');
-  }
+    history.push("/dashboard");
+  };
 
-  const handleEdit = e => {
+  const handleEdit = (e) => {
     e.preventDefault();
-    history.push(`/edit/${props.project.id}`)
-  }
+    history.push(`/edit/${props.project.id}`);
+  };
 
   return (
-    <section id="one" className="wrapper style2 spotlights">
-      <section>
-        <div className="content">
-          <div className="inner">
-              <div>Project Name: {props.project.project_name}</div>
-              <p>Project Description: {props.project.project_description}</p>
-              <div>Project Funding: {props.project.project_funding}</div>
-              <button onClick={handleEdit}>Edit</button>
-              <button onClick={handleDelete}>Delete</button>
+    <Project>
+      <div className="content">
+        <div className="inner">
+          <div className="title">
+            Project Name: {props.project.project_name}
           </div>
+          <p>Project Description: {props.project.project_description}</p>
+          <div>Project Funding: ${props.project.project_funding}</div>
+          <button onClick={handleEdit}>Edit</button>
+          <button onClick={handleDelete}>Delete</button>
         </div>
-      </section>
-    </section>
+      </div>
+    </Project>
   );
 }
 
 export default connect(null, { deleteProject, updateProject })(ProjectPanel);
+
+const Project = styled.section`
+  .inner {
+    /* background-color: #5052b5; */
+    margin-top: 10rem;
+    margin-bottom: 15px;
+    padding-bottom: 1rem;
+
+    .title {
+      display: flex;
+      justify-content: center;
+      font-size: 3rem;
+      @media (max-width: 1280px) {
+        width: 100%;
+      }
+      @media (max-width: 600px) {
+        font-size: 2rem;
+      }
+    }
+    p {
+      font-size: 2rem;
+      margin-bottom: 0;
+      @media (max-width: 600px) {
+        font-size: 1.5rem;
+      }
+    }
+    span {
+      color: rgba(255, 255, 255, 0.55);
+    }
+    div {
+      font-size: 2rem;
+      @media (max-width: 600px) {
+        font-size: 1.5rem;
+      }
+    }
+    button {
+      margin-top: 1rem;
+      height: 5rem;
+      font-size: 1rem;
+      @media (max-width: 600px) {
+        font-size: 0.8rem;
+      }
+    }
+  }
+`;
